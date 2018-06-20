750beea269cf528f1a2cee786f72e93601a771e6
Weber <weber2@kandagar.com>
2018-06-20 18:41:54 +0300

    work process 12%

--- palindrom.php
+++ palindrom.php
@@ -19,6 +19,7 @@
 - Аргентина манит негра
 - Sum summus mus
 */
+header('Content-Type: text/html; charset=utf-8');
 
 class Palindrom
 {
@@ -40,7 +41,9 @@ class Palindrom
     {
         if ($this->_is_text_string_palindrom()) {
             echo $this->_text_source;
-        }
+        } else {
+        	echo 1;
+		}
 
     }
 
@@ -53,7 +56,7 @@ class Palindrom
      */
     private function _get_first_word()
     {
-        return $this->_text_as_array[0];
+        return trim($this->_text_as_array[0]);
     }
 
     /**
@@ -63,7 +66,7 @@ class Palindrom
      */
     private function _get_last_word()
     {
-        return $this->_text_as_array[$this->_count_words_in_text - 1];
+        return trim($this->_text_as_array[$this->_count_words_in_text - 1]);
     }
 
     private function _is_text_string_palindrom()
@@ -71,12 +74,12 @@ class Palindrom
         $first_word = $this->_get_first_word();
         $last_word = $this->_get_last_word();
 
-        // Проверим слова на кол-во символов
-        if (count($first_word) === count($last_word)) {
-//            $compare_result = $this->_compare_words($first_word, $last_word);
-            $this->_compare_words($first_word, $last_word);
 
-//            if ($compare_result) return true;
+        // Проверим слова на кол-во символов
+        if (iconv_strlen($first_word, 'utf-8') === iconv_strlen($last_word, 'utf-8')) {
+			$compare_result = $this->_compare_words($first_word, $last_word);
+
+            if ($compare_result) return true;
         }
 
         return false;
@@ -86,12 +89,20 @@ class Palindrom
     {
         // Первый цикл идет с начала ->
         // Второй цикл с конца <-
+		$count = iconv_strlen($first_word, 'utf-8');
+		$i_reverse = $count - 1;
 
-        for ($f_i = 0; $f_i < strlen($first_word); $f_i++) {
-            for ($l_i = (strlen($last_word) - 1); $l_i > 0; $l_i-- ) {
+        for ($i = 0; $i < $count; $i++, $i_reverse--) {
+        	echo "<pre>"; var_dump($first_word[$i] , $last_word[$i_reverse]); echo "</pre>";
+        	echo "<pre>"; var_dump($first_word[$i] !== $last_word[$i_reverse]); echo "</pre>";
 
-            }
+
+//        	if ($first_word[$i] !== $last_word[$i_reverse]) {
+//        		return false;
+//			}
+        	
         }
+        return true;
     }
 }
 
@@ -107,3 +118,5 @@ foreach ($texts as $text) {
         $palindrom->show();
     }
 }
+
+phpinfo();
